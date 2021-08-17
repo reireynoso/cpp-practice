a06979eafed458fd47140bd2c7f2c2c4c1c1db1c
Reinald Reynoso <reireynoso@gmail.com>
2021-08-16 22:47:27 -0400

    move semantics

---
+++ cpp_files/move_semantics.cpp
@@ -0,0 +1,212 @@
+// Move semantics representation
+// Here's a function that takes an object of type T and returns an object of the same type T
+// This function uses call by value which means that when function is called, 
+// an object must be constructed to be used by the function, object has the scope of the function, considered temporary
+// since it returns value, another new object is constructed for the return value
+
+// Call by Value
+// T f(T o){
+//     return o;
+// }
+// T b = f(a);
+
+// Copy constructor
+// T::T(T & old){
+//     reset();
+//     copy_data(_a,old._a);
+//     copy_data(_b,old._b);
+//     copy_data(_c,old._c);
+//     copy_data(_d,old._d);
+// }
+
+// Move constructor (moving data instead of copying it) 
+// T::T(T && old){
+//     reset();
+//     _a = std::move(old._a);
+//     _b = std::move(old._b);
+//     _c = std::move(old._c);
+//     _d = std::move(old._d);
+//     old.reset();
+// }
+
+// Movin the data involves reassociating the data with the new object. This is accomplished with Rvalue reference
+// T & x lvalue reference (cannot be moveed)
+// T && y rvalue reference (may be moved)
+
+// UNDERSTANDING LVALUES AND RVALUES
+
+// assignment expression
+// (lvalue) a = b; (rvalue)
+
+// You're an r value if...
+// a teemporary value that is ready to expire
+// x = a + b;
+// literal value, pure rvalue, anything returned from a function that is not a reference 
+// x = 42;
+
+// USING STD::MOVE
+// c++ library provides template function called move
+// used to tell the compiler to use an object as it were an rvalue and then to move that value 
+// from source loc to dest loc without having to use copy
+
+// #include <cstdio>
+// #include <vector>
+// #include <string>
+// #include <utility>
+// using namespace std;
+
+// void message(const string & s) {
+//     puts(s.c_str());
+//     fflush(stdout);
+// }
+
+// void disp_t(vector<string> & v) {
+//     if(v.empty()) {
+//         puts("[empty]");
+//     } else {
+//         for( string s : v ) printf("%s ", s.c_str());
+//         puts("");
+//     }
+//     fflush(stdout);
+// }
+
+// template <typename T>
+// void swap(T & a, T & b) {
+//     message("swap()");
+//     T _tmp(std::move(a));
+//     a = std::move(b);
+//     b = std::move(_tmp);
+// }
+
+// int main() {
+//     vector<string> v1 = { "one", "two", "three", "four", "five" };
+//     vector<string> v2 = { "six", "seven", "eight", "nine", "ten" };
+    
+//     disp_t(v1);
+//     disp_t(v2);
+
+//     auto v3 = std::move(v2);
+
+//     disp_t(v1);
+//     disp_t(v2);
+//     disp_t(v3);
+//     return 0;
+// }
+
+
+// MOVE CONSTRUCTOR
+// in order to take adv of move semantics in your own classes, need to create move constructor
+
+#include <cstdio>
+#include <string>
+#include <utility>
+
+void message(const char * s) {
+    puts(s);
+    fflush(stdout);
+}
+
+// this version has messages to show which constructor (ctor) is being called.
+class Rational {
+    int _n = 0;
+    int _d = 1;
+    
+    // c_str buffer
+    static const int _bufsize = 128;
+    mutable char * _buf = nullptr;
+    
+public:
+    Rational() { reset(); message("default ctor"); }
+    Rational(const int & numerator) : _n(numerator), _d(1) { message("int ctor"); }
+    Rational(const int & numerator, const int & denominator) : _n(numerator), _d(denominator) { message("int,int ctor"); }
+    Rational(const Rational & other) : _n(other._n), _d(other._d) { message("copy ctor"); }
+    //move constructor, signarture, noexcept prevents the exceptions from leaving object in an unknown state
+    // whenever you have move constructor, must have no execept qualifier to prevent the exceptions from leaving src obj in unknown state
+    // compiler may ignore move constructor if no noexecpt is included
+    Rational(Rational &&) noexcept;
+    ~Rational();
+    void reset();
+    inline int numerator() const { return _n; }
+    inline int denominator() const { return _d; }
+    Rational & operator = (const Rational &);
+    Rational operator + (const Rational &) const;
+    Rational operator - (const Rational &) const;
+    Rational operator * (const Rational &) const;
+    Rational operator / (const Rational &) const;
+    operator std::string () const;
+    std::string string() const;
+    const char * c_str() const;
+};
+
+// move constuctor function 
+Rational::Rational(Rational && rhs) noexcept{
+    message("move constuctor");
+    _n = std::move(rhs._n);
+    _d = std::move(rhs._d);
+    rhs.reset();
+}
+
+Rational::~Rational() {
+    message("dtor");
+    reset();
+}
+
+void Rational::reset() {
+    _n = 0; _d = 1;
+    if(_buf) delete [] _buf;
+    _buf = nullptr;
+}
+
+Rational & Rational::operator = ( const Rational & rhs ) {
+    if( this != &rhs ) {
+        _n = rhs.numerator();
+        _d = rhs.denominator();
+    }
+    return *this;
+}
+
+Rational Rational::operator + ( const Rational & rhs ) const {
+    return Rational((_n * rhs._d) + (_d * rhs._n), _d * rhs._d);
+}
+
+Rational Rational::operator - ( const Rational & rhs ) const {
+    return Rational((_n * rhs._d) - (_d * rhs._n), _d * rhs._d);
+}
+
+Rational Rational::operator * ( const Rational & rhs ) const {
+    return Rational(_n * rhs._n, _d * rhs._d);
+}
+
+Rational Rational::operator / ( const Rational & rhs ) const {
+    return Rational(_n * rhs._d, _d * rhs._n);
+}
+
+Rational::operator std::string () const {
+    return string();
+}
+
+std::string Rational::string () const {
+    return std::string(c_str());
+}
+
+const char * Rational::c_str() const {
+    if(!_buf) _buf = new char[_bufsize]();
+    snprintf(_buf, _bufsize, "%d/%d", _n, _d);
+    return _buf;
+}
+
+int main() {
+    Rational a = 7;     // 7/1
+    Rational b(5, 3);   // 5/3
+    Rational c = b;     // copy ctor
+    Rational d = std::move(c);         // default ctor
+    
+    printf("a is: %s\n", a.c_str());
+    printf("b is: %s\n", b.c_str());
+    printf("c is: %s\n", c.c_str());
+    printf("d is: %s\n", d.c_str());
+    
+    printf("%s + %s is %s\n", a.c_str(), b.c_str(), Rational(a + b).c_str());
+    
+    return 0;
+}
